1335981318b717282bb6feecf4c3e8a99a84638b
Beelthazad <eduardocgcadiz@gmail.com>
2018-11-07 17:35:51 +0100

    Commit

--- ejercicio1.py
+++
@@ -1,9 +0,0 @@
-# Resultado de leer num enteros, reales o cadenas...
-num = int(input("Introduce un número\n"))
-print(num)
-char = input("Introduce un carácter\n")
-print(char)
-string = input("Introduce una cadena\n")
-print(string)
-
-
--- ejercicio3.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def areaCirculo(radio):
-    area = radio**2 * math.pi
-    longitud = 2 * math.pi * radio
-    return area, longitud
-
-r = int(input("Introduce radio...\n"))
-print(areaCirculo(r))
